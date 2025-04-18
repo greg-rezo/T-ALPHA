8ada2712035149918dbaf64347620c1edaa92ec1
Philipp <philipp@rezotx.com>
2025-04-17 23:35:57 -0700

    Adding debugging outputs to the model

--- src/t_alpha/models/full_model.py
+++ src/t_alpha/models/full_model.py
@@ -1,3 +1,4 @@
+import logging
 import torch
 from torch import nn
 from torch.nn import (
@@ -12,6 +13,8 @@ from torch_geometric.utils import to_dense_batch
 from t_alpha.models.egnn_encoder import EGNNEncoder
 from t_alpha.models.surface_convnet import SurfaceConvNet
 from t_alpha.models.utils import AttentionPooling, masked_mean_pool
+
+logger = logging.getLogger(__name__)
 
 
 class MetaModel(nn.Module):
